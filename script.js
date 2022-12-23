774d2e66d1041e4dada3bb7010d41f9d675851e5
Dwi-Wahyu <dwiwahyuilahi123@gmail.com>
2022-12-23 08:44:50 +0800

    Success Alert configuration

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ form.addEventListener("submit", async () => {
   const nama = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const message = document.getElementById("textAreaExample1").value;
+  const SuccessAlert = document.getElementById("SuccessAlert");
   const data = { nama, email, message };
   //   console.log(nama, email, message);
   fetch("https://wahil-api.vercel.app/v1/ContactMe/post/", {
@@ -14,6 +15,8 @@ form.addEventListener("submit", async () => {
   })
     .then((response) => response.json())
     .then((message) => {
-      console.log(message);
+      if (message.Success == "Post data success!") {
+        SuccessAlert.classList.remove("d-none");
+      }
     });
 });
